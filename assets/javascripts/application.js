6f8d2194b9ea7d7f2ef009c1bebfb4b11fb3e0ae
discoliam <discoliam@googlemail.com>
2013-09-24 17:59:22 +0100

    js to resize map, not really working...

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,7 +1,25 @@
 $( document ).ready(function() {
     
+    // VARS
+    var worldMap = $('#world-map');
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+
+    // Resize Map Function
+    function resizeMap() {
+      worldMap.width(windowWidth - 20 );
+      worldMap.height(windowHeight - 20 );
+    }
+
+    resizeMap();
+
+    // Draw Map
     $(function(){
       $('#world-map').vectorMap();
-    });
+    }); 
 
+});
+
+$(window).resize(function() {
+  resizeMap();
 });
